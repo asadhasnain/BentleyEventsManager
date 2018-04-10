78154b885a6583692dac620f66f1c16e0f530093
asadhasnain <asadhasnain@gmail.com>
2018-04-10 14:55:46 +0500

    Initial commit

---
+++ BentleyEventsManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BentleyEventsManager.Startup))]
+namespace BentleyEventsManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
